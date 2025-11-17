889f6d15bc215f50bd281f60acb1c5317549888b
Sergio <oliveira-sergio@outlook.com>
2025-11-17 20:52:07 +0000

    constructor modified

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract MyToken is ERC20, ERC20Permit {
-    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {}
+contract CA200Tokens is ERC20, ERC20Permit {
+
+    constructor() ERC20("CA_BlockChain_200Tokens", "CA200TK") ERC20Permit("CA_BlockChain_200Tokens") {
+        uint256 initialSupply = 200;
+         _mint(msg.sender, initialSupply);
+    }
 }
